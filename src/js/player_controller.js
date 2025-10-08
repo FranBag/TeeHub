432f6ef18f8b34a06d8acaaa64baf5effe61406b
FranBag <fbagneres@yahoo.com>
2025-10-07 23:52:26 -0300

    Añadido mensaje de jugador no encontrado

--- src/js/player_controller.js
+++ src/js/player_controller.js
@@ -4,19 +4,28 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const apiURL = `https://ddstats.tw/profile/json?player=${encodeURIComponent(playerName)}`;
 
+    const article = document.getElementById("player-article");
+    const notFound = document.getElementById("player-not-found");
+
     try {
         const response = await fetch(apiURL);
         const data = await response.json();
-        if(data.error) {
-            throw new Error(`No se ha encontrado un jugador con el nombre ${playerName}`);
+        
+        if (!data.name || data.error) {
+            throw new Error("Jugador no encontrado");
         }
 
         document.getElementById("player-name").textContent = data.name;
         document.getElementById("player-clan").textContent = `Clan: ${data.clan || "Sin clan"}`;
         document.getElementById("player-points").textContent = `Puntuación: ${data.points}`;
 
+        article.style.display = "flex";
+        notFound.style.display = "none";
+
         console.log("Datos del jugador:", data);
     } catch (error) {
         console.error(error);
+        article.style.display = "none";
+        notFound.style.display = "block";
     }
 });
