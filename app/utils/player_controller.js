1a992b08f4612da7cf513d6671c5cdb39eebace4
FranBag <fbagneres@yahoo.com>
2025-11-18 17:49:47 -0300

    Añadido skins y flags

--- app/utils/player_controller.js
+++ app/utils/player_controller.js
@@ -1,4 +1,18 @@
+async function loadCountryFlagsMap() {
+    const response = await fetch("../utils/countryflags.json");
+    return await response.json();
+}
+
+function getFlagUrl(code, map) {
+    if (code == null || code < 0) return null;
+    const filename = map[code];
+    if (!filename) return null;
+
+    return `https://raw.githubusercontent.com/ddnet/ddnet/master/data/countryflags/${filename}`;
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
+    const map = await loadCountryFlagsMap();
     const params = new URLSearchParams(window.location.search);
     const playerName = params.get("player_name");
 
@@ -19,10 +33,28 @@ document.addEventListener("DOMContentLoaded", async () => {
         document.getElementById("player-clan").textContent = `Clan: ${data.clan || "Sin clan"}`;
         document.getElementById("player-points").textContent = `Puntuación: ${data.points}`;
 
+        const skinUrl = `https://teedata.net/api/skin/render/name/${encodeURIComponent(data.skin_name)}`;
+
+        const imgSkin = document.getElementById("player-skin");
+        imgSkin.src = skinUrl;
+        imgSkin.onerror = () => {
+            imgSkin.src = "../../public/assets/images/player_skin_placeholder.png";
+        };
+
+        const imgFlag = document.getElementById("player-countryflag");
+
+        const flagUrl = getFlagUrl(data.country, map);  
+
+        if (flagUrl) {
+            imgFlag.src = flagUrl;
+            imgFlag.style.display = "block";
+        } else {
+            imgFlag.src = "../../public/assets/images/country_flag_placeholder.png";
+        }
+        console.log(data);
+
         article.style.display = "flex";
         notFound.style.display = "none";
-
-        console.log("Datos del jugador:", data);
     } catch (error) {
         console.error(error);
         article.style.display = "none";
